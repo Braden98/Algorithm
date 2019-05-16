14fa0c8cb601d5c621ed8cc405fdef2fa2e46dcf
Ubique0305 <1071763478@qq.com>
2019-05-16 10:38:59 +0800

    加入了排序用工具 ，包括生成随机数列、复制数组、打印数组  * 判断相等 以及最重要的交换数列中两个数的位置

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,19 @@
+package Algorithm.Sort;
+
+/**
+ * @Description
+ * @Author ubique
+ * @Date 2019/5/16 10:27 AM
+ */
+
+public class BubbleSort {
+    public static void bubbleSort(int[] list){
+        for(int k=1;k<list.length;k++){
+            for(int i=0;i<list.length-k;i++){
+                if(list[i]>list[i+1]){
+
+                }
+            }
+        }
+    }
+}
