5c772bf8dbd18334db331168b1ec4e83c42f868a
Ubique0305 <1071763478@qq.com>
2019-05-20 21:50:14 +0800

    增加二叉树的{前序,中序,后序}叉乘{递归，非递归}遍历

---
+++ Binarytree/Node.java
@@ -0,0 +1,16 @@
+package Algorithm.Binarytree;
+
+/**
+ * @Description 普通二叉树的节点定义
+ * @Author ubique
+ * @Date 2019/5/20 9:46 PM
+ */
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
---
+++ Binarytree/Traversal.java
@@ -0,0 +1,120 @@
+package Algorithm.Binarytree;
+
+import java.util.Stack;
+
+/**
+ * @Description 二叉树的{前序,中序,后序}叉乘{递归，非递归}遍历
+ * @Author ubique
+ * @Date 2019/5/20 9:44 PM
+ */
+
+public class Traversal {
+    public static void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    public static void posOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void preOrderUnRecur(Node head) {
+        System.out.print("pre-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.print(head.value + " ");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void inOrderUnRecur(Node head) {
+        System.out.print("in-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.print(head.value + " ");
+                    head = head.right;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void posOrderUnRecur1(Node head) {
+        System.out.print("pos-order: ");
+        if (head != null) {
+            Stack<Node> s1 = new Stack<Node>();
+            Stack<Node> s2 = new Stack<Node>();
+            s1.push(head);
+            while (!s1.isEmpty()) {
+                head = s1.pop();
+                s2.push(head);
+                if (head.left != null) {
+                    s1.push(head.left);
+                }
+                if (head.right != null) {
+                    s1.push(head.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.print(s2.pop().value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void posOrderUnRecur2(Node h) {
+        System.out.print("pos-order: ");
+        if (h != null) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.push(h);
+            Node c = null;
+            while (!stack.isEmpty()) {
+                c = stack.peek();
+                if (c.left != null && h != c.left && h != c.right) {
+                    stack.push(c.left);
+                } else if (c.right != null && h != c.right) {
+                    stack.push(c.right);
+                } else {
+                    System.out.print(stack.pop().value + " ");
+                    h = c;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+}
