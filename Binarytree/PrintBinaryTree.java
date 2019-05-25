6f4a0bf2aa6915f49ccdb1c385fa476cdc7ca232
Ubique0305 <1071763478@qq.com>
2019-05-25 22:35:28 +0800

    增加 二叉树打印问题

---
+++ Binarytree/PrintBinaryTree.java
@@ -0,0 +1,38 @@
+package Algorithm.Binarytree;
+
+/**
+ * @Description 二叉树的打印
+ * @Author ubique
+ * @Date 2019/5/25 10:34 PM
+ */
+
+public class PrintBinaryTree {
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+}
