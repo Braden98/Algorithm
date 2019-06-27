a9d95c32f68415d5214a3237a3ffbcdd4a5c662e
Ubique0305 <1071763478@qq.com>
2019-06-27 22:29:55 +0800

    MinLengthForSort

---
+++ Array/MinLengthForSort.java
@@ -0,0 +1,37 @@
+package Algorithm.Array;
+
+/**
+ * @Description  最小的需要排序的部分
+ * @Author ubique
+ * @Date 2019/6/27 10:29 PM
+ */
+
+public class MinLengthForSort {
+    public static int getMinLength(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int min = arr[arr.length - 1];
+        int noMinIndex = -1;
+        for (int i = arr.length - 2; i != -1; i--) {
+            if (arr[i] > min) {
+                noMinIndex = i;
+            } else {
+                min = Math.min(min, arr[i]);
+            }
+        }
+        if (noMinIndex == -1) {
+            return 0;
+        }
+        int max = arr[0];
+        int noMaxIndex = -1;
+        for (int i = 1; i != arr.length; i++) {
+            if (arr[i] < max) {
+                noMaxIndex = i;
+            } else {
+                max = Math.max(max, arr[i]);
+            }
+        }
+        return noMaxIndex - noMinIndex + 1;
+    }
+}
