60ca0018b46fd5aa7c6e31b09b5bfc47221149d2
Ubique0305 <1071763478@qq.com>
2019-06-29 22:44:54 +0800

    滑动窗口

---
+++ Array/SubArrayMaxSum.java
@@ -0,0 +1,31 @@
+package Algorithm.Array;
+
+/**
+ * @Description 滑动窗口
+ *  经典问题
+ * @Author ubique
+ * @Date 2019/6/29 10:44 PM
+ */
+
+public class SubArrayMaxSum {
+    public static int maxSum(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for (int i = 0; i != arr.length; i++) {
+            cur += arr[i];
+            max = Math.max(max, cur);
+            cur = cur < 0 ? 0 : cur;
+        }
+        return max;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i != arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
