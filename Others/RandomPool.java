dafdaea77ac4e969954b40b99b8e6b8f22a5c47f
Ubique0305 <1071763478@qq.com>
2019-07-05 22:42:20 +0800

    randompool

---
+++ Others/RandomPool.java
@@ -0,0 +1,51 @@
+package Algorithm.Others;
+
+import java.util.HashMap;
+
+/**
+ * @Description 随机池问题
+ * @Author ubique
+ * @Date 2019/7/5 10:41 PM
+ */
+
+public class RandomPool {
+    public static class Pool<K> {
+        private HashMap<K, Integer> keyIndexMap;
+        private HashMap<Integer, K> indexKeyMap;
+        private int size;
+
+        public Pool() {
+            this.keyIndexMap = new HashMap<K, Integer>();
+            this.indexKeyMap = new HashMap<Integer, K>();
+            this.size = 0;
+        }
+
+        public void insert(K key) {
+            if (!this.keyIndexMap.containsKey(key)) {
+                this.keyIndexMap.put(key, this.size);
+                this.indexKeyMap.put(this.size++, key);
+            }
+        }
+
+        public void delete(K key) {
+            if (this.keyIndexMap.containsKey(key)) {
+                int deleteIndex = this.keyIndexMap.get(key);
+                int lastIndex = --this.size;
+                K lastKey = this.indexKeyMap.get(lastIndex);
+                this.keyIndexMap.put(lastKey, deleteIndex);
+                this.indexKeyMap.put(deleteIndex, lastKey);
+                this.keyIndexMap.remove(key);
+                this.indexKeyMap.remove(lastIndex);
+            }
+        }
+
+        public K getRandom() {
+            if (this.size == 0) {
+                return null;
+            }
+            int randomIndex = (int) (Math.random() * this.size);
+            return this.indexKeyMap.get(randomIndex);
+        }
+
+    }
+}
