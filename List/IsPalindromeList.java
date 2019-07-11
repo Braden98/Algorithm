3f839bb7e90f3770621fadf23dfc4fe9f2315dd0
Ubique0305 <1071763478@qq.com>
2019-07-11 22:27:56 +0800

    反转部分链表

---
+++ List/IsPalindromeList.java
@@ -0,0 +1,10 @@
+package Algorithm.List;
+
+/**
+ * @Description
+ * @Author ubique
+ * @Date 2019/7/11 10:27 PM
+ */
+
+public class IsPalindromeList {
+}
---
+++ List/ReversePartList.java
@@ -0,0 +1,58 @@
+package Algorithm.List;
+
+/**
+ * @Description 反转部分单链表
+ * @Author ubique
+ * @Date 2019/7/11 10:26 PM
+ */
+
+public class ReversePartList {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node reversePart(Node head, int from, int to) {
+        int len = 0;
+        Node node1 = head;
+        Node fPre = null;
+        Node tPos = null;
+        while (node1 != null) {
+            len++;
+            fPre = len == from - 1 ? node1 : fPre;
+            tPos = len == to + 1 ? node1 : tPos;
+            node1 = node1.next;
+        }
+        if (from > to || from < 1 || to > len) {
+            return head;
+        }
+        node1 = fPre == null ? head : fPre.next;
+        Node node2 = node1.next;
+        node1.next = tPos;
+        Node next = null;
+        while (node2 != tPos) {
+            next = node2.next;
+            node2.next = node1;
+            node1 = node2;
+            node2 = next;
+        }
+        if (fPre != null) {
+            fPre.next = node1;
+            return head;
+        }
+        return node1;
+    }
+
+    public static void printLinkedList(Node head) {
+        System.out.print("Linked List: ");
+        while (head != null) {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
