d536ef9965e759bc87f4e02eda2fb8ed6b908e25
Ubique0305 <1071763478@qq.com>
2019-07-22 22:39:16 +0800

    public static int ways1(int N, int M, int K, int P) {

---
+++ Leetcode/Solution.java
@@ -0,0 +1,26 @@
+package Algorithm.Leetcode;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author ubique
+ * @Date 2019/7/19 11:28 AM
+ */
+
+
+//  public class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode(int x) { val = x; }
+//  }
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        ans.addAll(inorderTraversal(root.left));
+    }
+}
